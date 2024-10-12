465a9ef220379500fd574aeb2c6d0447b67aaf9a
qoride <qoride@gmail.com>
2024-10-12 22:36:28 +0000

    imported cin edge cases from project 1, finished

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <limits>
+#include <vector>
 
 using namespace std;
 
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main(){
-    int num1 = 1;
-    int num2 = 2;
-    int temp;
+    int val;
+    vector<int> input;
 
-    cout << "Initial Values:";
-    cout << "\nnum1 = " << num1 << "\nnum2 = " << num2;
+    cout << "Enter two values seperated by a space: ";
+    while(input.size() < 2){
+        while(cin >> val){
+            if(cin.good())input.push_back(val);
+            if(input.size()>=2)break;
+        }
+        if(cin.fail()){
+            cin.clear();
+            cout << "\nERROR: A value was not accepted, try again: ";
+            input.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            cin.clear();
+        }
+    }
 
-    temp = num1;
-    num1 = num2;
-    num2 = temp;
-    
-    cout << "\n\nNew Values:";
-    cout << "\nnum1 = " << num1 << "\nnum2 = " << num2;
+    cout << "\nYou entered the values num1 = " << input.at(0) << ", and num2 = " << input.at(1)
+    << "\n\nSwapping...";
+
+    swap(&input.at(0),&input.at(1));
+
+    cout << "\n\nNew values are num1 = " << input.at(0) << ", and num2 = " << input.at(1);
 
     return 0;
 }
